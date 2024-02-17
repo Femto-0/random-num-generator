5ef8b4d7b2d7ee32552fd314510211dd71bab7d7
Ritesh Shah <arits.king74@gmail.com>
2024-02-17 15:06:18 -0600

    added a new method to make a list of all cities and total number of houses. Also, all cities are separated by a -- whenever they fall under a new state

--- src/main/java/RandomNumGenerator.java
+++ src/main/java/RandomNumGenerator.java
@@ -3,6 +3,8 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Objects;
 
 public class RandomNumGenerator {
     String fileName= "src/main/java/list.txt";
@@ -17,17 +19,39 @@ public class RandomNumGenerator {
            islandName=arr[0];
            if(!(island.contains(islandName))&& islandName!=null) {
                island.add(islandName);
-               island.remove(null);
            }
 
         }
-
         return island;
     }
+    public ArrayList<String> createCityAndHouseList(ArrayList<String> arrList) throws IOException {
+        ArrayList<String> cityList= new ArrayList<>();
+        BufferedReader br= new BufferedReader(new FileReader(fileName));
+       String firstWord;
+       String[] arr ;
+        int i=0;
+       while((firstWord=br.readLine())!=null) {
+           arr = firstWord.split("\\s");
+
+               if(Objects.equals(arrList.get(i), arr[0])) {
+                   cityList.add(arr[1]);
+                   cityList.add(arr[2]);
+               }else{
+                   cityList.add("--");
+                   cityList.add(arr[1]);
+                   cityList.add(arr[2]);
+                   i++;
+               }
+       }
+       br.close();
+        return cityList;
+    }
+
 
     public static void main(String[] args) throws IOException {
         RandomNumGenerator rng= new RandomNumGenerator();
         System.out.println(rng.readList());
+        System.out.println(rng.createCityAndHouseList(rng.readList()));
         System.out.println(rng.readList().size());
     }
 }
