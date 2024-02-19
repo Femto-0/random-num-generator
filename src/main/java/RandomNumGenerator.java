156cc855dc38942ddc2a85d39267ad3ff270ecea
Ritesh Shah <arits.king74@gmail.com>
2024-02-19 11:50:32 -0600

    added few if statements and now we can generate required number of random house numbers that is greater than total number of cities. Previously, we were able to< genearte random house numbers= total number of cities> but now < n no. of random random house numbers> can be generated

--- src/main/java/RandomNumGenerator.java
+++ src/main/java/RandomNumGenerator.java
@@ -1,5 +1,4 @@
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -37,7 +36,7 @@ public class RandomNumGenerator {
             if (Objects.equals(arrList.get(i), arr[0])) {
                 cityList.add(arr[1]);
             } else {
-                cityList.add("--");
+                cityList.add("--");// adding -- here will help us separate cities of different islands.
                 cityList.add(arr[1]);
                 i++;
             }
@@ -58,7 +57,7 @@ public class RandomNumGenerator {
             if (Objects.equals(arrList.get(i), arr[0])) {
                 cityList.add(arr[2]);
             } else {
-                cityList.add("--");
+                cityList.add("--"); // adding -- here will help us separate houses of different islands.
                 cityList.add(arr[2]);
                 i++;
             }
@@ -68,8 +67,8 @@ public class RandomNumGenerator {
     }
 
 
-    public void findRandomNum(ArrayList<String> readList, ArrayList<String> createCityList, ArrayList<String> createHouseList) {
-        ArrayList<Integer> randomList = new ArrayList<>();
+    public void findRandomNum(int preSpecifiedNum, ArrayList<String> readList, ArrayList<String> createCityList, ArrayList<String> createHouseList) {
+        int[] randomList = new int[preSpecifiedNum];
         int totalHouseInAnIsland = 0;
 
         // Calculate the total number of houses in an island
@@ -79,29 +78,39 @@ public class RandomNumGenerator {
             }
         }
 
-       // int num = preSpecifiedNumber / readList.size();
 
-        // Ensure there are enough cities for random selection
-       // if (num <= totalHouseInAnIsland) {
-            // Generate random numbers for the specified number of times
-            for (int i = 0; i < totalHouseInAnIsland; i++) {
+        // Generate random numbers for the specified number of times
+        int counter = 0;
+        for (int i = 0; i < preSpecifiedNum; i++) {
+            if (counter < preSpecifiedNum) {
+                if (counter != preSpecifiedNum - 1 && i == totalHouseInAnIsland - 1) {
+                    i = 0;
+                }
                 int maxNum = Integer.parseInt(createHouseList.get(i));
                 int randomNum = (int) (Math.random() * (maxNum - 1)) + 1;
-                randomList.add(randomNum);
-          //  }
+                randomList[counter] = randomNum;
+                counter++;
+            }
         }
+        System.out.println(Arrays.toString(randomList));
         System.out.println("Random house numbers that can be surveyed across different islands are:");
 
         // Print the random numbers for each island
-        for (int i = 0; i < readList.size(); i++) {
-            System.out.println("For Island: " + readList.get(i));
-            for (int j = 0; j < totalHouseInAnIsland && j < randomList.size(); j++) {
-                System.out.println("For city " + createCityList.get(j) + " random house number is: " + randomList.get(j));
+        for (String s : readList) {
+            System.out.println("For Island: " + s);
+            int secondCounter = 0;
+            for (int j = 0; j < totalHouseInAnIsland && j < randomList.length; j++) {
+                if (secondCounter < preSpecifiedNum) {
+                    if (secondCounter != preSpecifiedNum - 1 && j == totalHouseInAnIsland - 1) {
+                        j = 0;
+                        System.out.println("--------------------------------------------------");
+                    }
+                    System.out.println(secondCounter + 1 + ". For city " + createCityList.get(j) + " random house number is: " + randomList[secondCounter]);
+                    secondCounter++;
+                }
             }
         }
     }
-
-
 
 
     public static void main(String[] args) throws IOException {
@@ -112,6 +121,6 @@ public class RandomNumGenerator {
         System.out.println(rng.readList().size());
         System.out.println("--------------------------");
         //here, pre-specified number refers to the number of cities that you need random house number from.
-        rng.findRandomNum(9, rng.readList(), rng.createCityList(rng.readList()),rng.createHouseList(rng.readList()));
+        rng.findRandomNum(100, rng.readList(), rng.createCityList(rng.readList()), rng.createHouseList(rng.readList()));
     }
 }
